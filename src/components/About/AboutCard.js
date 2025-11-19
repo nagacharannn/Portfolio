8f270c0de8d015c400df4a95ab2bd10805a15008
prasanthirapuru <prasanthirapuru91@gmail.com>
2025-11-19 12:47:48 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
@@ -8,32 +9,37 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Prasanthi Rapuru  </span>
-            from <span className="purple"> Nellore, Andhra Pradesh, India.</span>
+            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
+            from <span className="purple">Bhubaneswar, India</span>.
             <br />
-            Final Year Computer Science Student | Aspiring Software Developer.
+            I’m currently working as a{" "}
+            <span className="purple">Software Developer</span> at{" "}
+            <span className="purple">Juspay</span>.
+            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
+            <span className="purple">Mathematics and Computing</span> from{" "}
+            <span className="purple">BIT Mesra</span>.
             <br />
-            B.Tech in Computer Science, Sri Venkateswara College of Engineering.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Outside of coding, I love engaging in activities that keep me
+            creative and inspired:
           </p>
+
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Games 🎮
             </li>
             <li className="about-activity">
-              <ImPointRight /> Photography
+              <ImPointRight /> Writing Tech Blogs ✍️
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Traveling and Exploring New Places 🌍
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Prasanthi</footer>
+          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
